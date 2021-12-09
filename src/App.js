4b58fe047bd6be43d59d7deefbbeb2f21d928ddf
coding-with-nilesh <95519505+coding-with-nilesh@users.noreply.github.com>
2021-12-09 13:08:03 +0530

    Style changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,102 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { googleLoadScript } from './googleLoadScript';
+const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 function App() {
+  
+  const [gapi, setGapi] = useState();
+  const [googleAuth, setGoogleAuth] = useState();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [imageUrl, setImageUrl] = useState();
+  
+  const onSuccess = (googleUser) => {
+    setIsLoggedIn(true);
+    const profile = googleUser.getBasicProfile();
+    setName(profile.getName());
+    setEmail(profile.getEmail());
+    setImageUrl(profile.getImageUrl());
+  };
+  
+  const onFailure = () => {
+    setIsLoggedIn(false);
+  }
+  
+  const logOut = () => {
+    (async() => {
+      await googleAuth.signOut();
+      setIsLoggedIn(false);
+      renderSigninButton(gapi);
+    })();
+  };
+  
+  const renderSigninButton = (_gapi) => {
+    _gapi.signin2.render('google-signin', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': onSuccess,
+      'onfailure': onFailure 
+    });
+  }
+  
+  
+  useEffect(() => {
+    
+    //window.gapi is available at this point
+    window.onGoogleScriptLoad = () => {
+     
+      const _gapi = window.gapi;
+      setGapi(_gapi);
+      
+      _gapi.load('auth2', () => {
+        (async () => { 
+          const _googleAuth = await _gapi.auth2.init({
+           client_id: googleClientId
+          });
+          setGoogleAuth(_googleAuth);
+          renderSigninButton(_gapi);
+        })();
+      });
+    }
+    
+    //ensure everything is set before loading the script
+     googleLoadScript();
+    
+  }, []);
+  
+  
   return (
     <div className="App">
-      Social Login
+      <header className="App-header">
+        {!isLoggedIn &&
+          <div id="google-signin"><LogInPage/></div>
+        }
+        
+        {isLoggedIn &&
+          <div>
+            <div>
+              <img src={imageUrl} />
+            </div>
+            <div>{name}</div>
+            <div>{email}</div>
+            <button className='btn-primary' onClick={logOut}>Log Out</button>
+          </div>
+        }
+      </header>
     </div>
   );
 }
 
 export default App;
+
+function LogInPage(){
+  return (
+    <div>LoginPage</div>
+  );
+}
+
